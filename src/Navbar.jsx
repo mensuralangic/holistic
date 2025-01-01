7d9996116787e2a41c908e3c9095c586c3f3056d
unknown <mensuralangic@gmail.com>
2025-01-02 00:15:12 +0100

    added homepage and updated navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,10 +5,13 @@ import Icon from './Icon';
 const Navbar = () => {
 
     return (
-        <div className="bg-emerald-600 px-24 pt-12 pb-16 text-white">
+        <div className="bg-emerald-600 px-24 pt-12 pb-16 text-white sticky top-0 ">
             <div className="container mx-auto flex items-start justify-between">
                 <div className="text-7xl">
-                    LOGO
+                    <MyButton 
+                        to="/home"
+                        title='LOGO'
+                    />
                 </div>
                 <div className="flex gap-10">
                     <MyButton
--- src/main.jsx
+++ src/main.jsx
@@ -7,17 +7,19 @@ import About from './About';
 import Blog from './Blog';
 import Navbar from './Navbar';
 import Contact from './Contact';
+import Home from './Home';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
